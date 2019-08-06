49becbc16d807a57a777bae51269867e65e5ccac
GaborPap <ppgabor@gmail.com>
2019-08-06 20:29:47 +0200

    Implemented filtering. Can filter by supllier, category and both.

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -28,14 +28,26 @@ public class Initializer implements ServletContextListener {
         supplierDataStore.add(amazon);
         Supplier lenovo = new Supplier("Lenovo", "Computers");
         supplierDataStore.add(lenovo);
+        Supplier huawei = new Supplier("Huawei", "Phones");
+        supplierDataStore.add(huawei);
 
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(tablet);
+        ProductCategory pc = new ProductCategory("PC", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display2.");
+        productCategoryDataStore.add(pc);
+        ProductCategory phone = new ProductCategory("PHONE", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display2.");
+        productCategoryDataStore.add(phone);
+
 
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+        productDataStore.add(new Product("PCPCPCPC Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", pc, amazon));
+        productDataStore.add(new Product("PC2", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", pc, amazon));
+        productDataStore.add(new Product("PHONE1", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", pc, huawei));
+        productDataStore.add(new Product("Amazon2 Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+
     }
 }
--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -5,6 +5,8 @@ import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.Product;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -14,27 +16,53 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends HttpServlet {
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDaoMem supplierDaoMem = SupplierDaoMem.getInstance();
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        context.setVariable("category", productCategoryDataStore.find(1));
-        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+        context.setVariable("category", productCategoryDataStore.getAll());
+   //     context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+        context.setVariable("products", filterProducts(req.getParameter("categories"), req.getParameter("suppliers")));
+        context.setVariable(("suppliers"), supplierDaoMem.getAll());
+        context.setVariable("filterCatId", req.getParameter("categories"));
+        context.setVariable("filterSupptId", req.getParameter("suppliers"));
         // // Alternative setting of the template context
         // Map<String, Object> params = new HashMap<>();
         // params.put("category", productCategoryDataStore.find(1));
         // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
         // context.setVariables(params);
+
+        System.out.println(req.getParameter("categories"));
         engine.process("product/index.html", context, resp.getWriter());
+    }
+
+    private List<Product> filterProducts(String catId, String suppId){
+
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDaoMem supplierDaoMem = SupplierDaoMem.getInstance();
+        List<Product> temp = productDataStore.getAll();
+        if ((catId==null) || catId.equals("-1")){
+          //  return productDataStore.getAll();
+            temp = productDataStore.getAll();
+        }else
+            temp = productDataStore.getBy(productCategoryDataStore.find(Integer.valueOf(catId)));
+           // return productDataStore.getBy(productCategoryDataStore.find(Integer.valueOf(catId)));
+
+        if ((suppId==null) || (suppId.equals("-1")))
+            return temp;
+        return temp.stream().filter(t -> t.getSupplier().equals(supplierDaoMem.find(Integer.valueOf(suppId)))).collect(Collectors.toList());
     }
 
 }
