0ea5d8ba05ed007363d250a5e92b5e58e57528ac
GaborPap <ppgabor@gmail.com>
2019-08-22 11:28:36 +0200

    Added beforeall

--- src/main/test/com/codecool/shop/dao/ProductCategoryDaoTest.java
+++ src/main/test/com/codecool/shop/dao/ProductCategoryDaoTest.java
@@ -1,10 +1,17 @@
 package com.codecool.shop.dao;
 
+import com.codecool.shop.database.DataBaseHandler;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductCategoryDaoTest {
+
+    @BeforeAll
+    private void setup(){
+        DataBaseHandler.setDatabase("test.properties");
+    }
 
     @Test
     public void testAddCategoryToTable() {
