cd8a13e4ed66ac5db4537cf691187891010f41b9
GaborPap <ppgabor@gmail.com>
2019-08-22 13:15:17 +0200

    Implemented Productcategory and Supplier tests

--- src/main/test/com/codecool/shop/dao/ProductCategoryDaoTest.java
+++ src/main/test/com/codecool/shop/dao/ProductCategoryDaoTest.java
@@ -1,35 +1,56 @@
 package com.codecool.shop.dao;
 
+import com.codecool.shop.Util;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoJDBC;
 import com.codecool.shop.database.DataBaseHandler;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import com.codecool.shop.model.ProductCategory;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ProductCategoryDaoTest {
+    private ProductCategoryDaoJDBC productCategoryDataStore = new ProductCategoryDaoJDBC();
 
     @BeforeAll
-    private void setup(){
+    private static void setup() {
         DataBaseHandler.setDatabase("test.properties");
     }
 
     @Test
-    public void testAddCategoryToTable() {
-
+    @Order(1)
+    void testAddCategoryToTable() {
+        int id = Util.getNextIdFromTable("categories");
+        ProductCategory productCategory = new ProductCategory(id, "Test category", "Testing", "Testing");
+        productCategoryDataStore.add(productCategory);
+        assertNotNull(productCategoryDataStore.find(id));
     }
 
     @Test
-    public void testFindCategoryById() {
-
+    @Order(2)
+    void testFindCategoryById() {
+        int id = Util.getNextIdFromTable("categories") - 1;
+        assertNotNull(productCategoryDataStore.find(id));
     }
 
     @Test
-    public void testRemoveCategoryById() {
-
+    @Order(4)
+    void testFindCategoryNotFounded() {
+        assertNull(productCategoryDataStore.find(-1));
     }
 
     @Test
-    public void testGetAllCategories() {
+    @Order(5)
+    void testRemoveCategoryById() {
+        int id = Util.getNextIdFromTable("categories") - 1;
+        productCategoryDataStore.remove(id);
+        assertNull(productCategoryDataStore.find(id));
+    }
+
+    @Test
+    @Order(6)
+    void testGetAllCategories() {
+        assertEquals(4, productCategoryDataStore.getAll().size());
 
     }
 
